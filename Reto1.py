90b9e527b8d7f52cc39d1e281a19993059988261
HaiderParra42 <haiderparra224@unisangil.edu.co>
2025-03-11 18:51:04 -0500

    Update Reto1.py

--- Reto1.py
+++ Reto1.py
@@ -1,3 +1,8 @@
-Hola PROFE :)
+import math
+consumo_anual =float(input("ingrese el consumo anual de electricidad en kWh :"))
+horas_sol = float(input("ingrese las horas promedio del sol por dia :"))
+eficiencia =float(input("ingrese la eficiencia del panel solar en porcentaje (ej= 15 para 15% ) :"))
+area_panel =float(input("ingrese la superficie promedio de un panel solar en m2 :"))
+radiacion =float(input("ingrese la radiacion solar promedio por metro cuadrado en kWh/m2/dia :"))
 
 
