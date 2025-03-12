4677cb9a4fd260759e49c6e3072fa8e19cd8fd3f
Santiago Monroy <johandmonroyAunisangil.edu.co>
2025-03-12 12:28:46 -0500

    Quiz

--- Reto1.py
+++ Reto1.py
@@ -1,7 +1,7 @@
 import math
 consumo_anual =float(input("ingrese el consumo anual de electricidad en kWh :"))
 horas_sol = float(input("ingrese las horas promedio del sol por dia :"))
-eficiencia =float(input("ingrese la eficiencia del panel solar en porcentaje (ej= 15 para 15% ) :"))
+eficiencia = float(input("ingrese la eficiencia del panel solar en porcentaje (ej= 15 para 15% ) :")) / 100
 area_panel =float(input("ingrese la superficie promedio de un panel solar en m2 :"))
 radiacion =float(input("ingrese la radiacion solar promedio por metro cuadrado en kWh/m2/dia :"))
 potencia_diaria= area_panel*radiacion* eficiencia
