05123b6b0d0d9e2ecbfb3a99d8ea58ab4220f5e0
davidc200423 <davidc200423@gmail.com>
2025-03-11 19:07:59 -0500

    Update Reto1.py

--- Reto1.py
+++ Reto1.py
@@ -4,5 +4,9 @@ horas_sol = float(input("ingrese las horas promedio del sol por dia :"))
 eficiencia =float(input("ingrese la eficiencia del panel solar en porcentaje (ej= 15 para 15% ) :"))
 area_panel =float(input("ingrese la superficie promedio de un panel solar en m2 :"))
 radiacion =float(input("ingrese la radiacion solar promedio por metro cuadrado en kWh/m2/dia :"))
+potencia_diaria= area_panel*radiacion* eficiencia
+potencia_anual = potencia_diaria*365
+num_paneles = consumo_anual/potencia_anual
+area_total= num_paneles*area_panel
 
 
